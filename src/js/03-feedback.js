88cdf603562ea99caf0eb7f892b417d9a0f2afbf
Evalac <maslenokprograming@gmail.com>
2024-07-11 11:47:08 +0300

    qqqq

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,21 @@
 const formEl = document.querySelector('.feedback-form');
 
 console.log(formEl);
-formEl.addEventListener('submit', onInput);
+formEl.addEventListener('click', onInput);
 
 function onInput(evt) {
   evt.preventDefault();
   const formElement = evt.currentTarget.elements;
   console.log(formElement);
+  console.dir(evt.target);
+  if (evt.target.type !== 'email') {
+    return;
+  }
+  console.log(evt.target.value);
+  if (evt.target.nodeName !== 'TEXTAREA') {
+    return;
+  }
+  console.log(evt.target.value);
 }
 
 //maslenok@mail.com
