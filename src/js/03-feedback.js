68a67f9ff44e527f34bd6fb67588ca30b439c45c
Evalac <maslenokprograming@gmail.com>
2024-07-11 11:10:53 +0300

    input

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
+const formEl = document.querySelector('.feedback-form');
 
+console.log(formEl);
+formEl.addEventListener('submit', onInput);
+
+function onInput(evt) {
+  evt.preventDefault();
+  const formElement = evt.currentTarget.elements;
+  console.log(formElement);
+}
+
+//maslenok@mail.com
