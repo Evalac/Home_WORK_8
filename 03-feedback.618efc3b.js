f9514dc804d37dc2a3c0ec18ecc0c6ef579c615e
Evalac <102866941+Evalac@users.noreply.github.com>
2024-07-13 15:56:54 +0000

    Deploying to gh-pages from @ Evalac/home_work_8@5a9bd1029a40ce4f88ed001cbb6c3144b210ea5b 🚀

---
+++ 03-feedback.618efc3b.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("input",(function(e){e.preventDefault(),e.currentTarget.elements,"email"===e.target.type?"TEXTAREA"===e.target.nodeName||console.log("textarea",e.target.value):console.log("mail",e.target.value)}));
+//# sourceMappingURL=03-feedback.618efc3b.js.map
--- 03-feedback.6b7c0a2f.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("input",(function(e){e.preventDefault(),e.currentTarget.elements,console.dir(e.target),"email"===e.target.type&&(console.log(e.target.value),"TEXTAREA"===e.target.nodeName&&console.log(e.target.value))}));
-//# sourceMappingURL=03-feedback.6b7c0a2f.js.map
