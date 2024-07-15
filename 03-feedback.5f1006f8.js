2dfe1656c56047cc526084c616c4ea59b08df02a
Evalac <102866941+Evalac@users.noreply.github.com>
2024-07-15 16:14:18 +0000

    Deploying to gh-pages from @ Evalac/home_work_8@d6fa73927db8471ec60b77166ed0333e0570c07b 🚀

--- 03-feedback.5f1006f8.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".feedback-form textarea")};e.form.addEventListener("submit",(function(e){})),e.textarea.addEventListener("input",(function(e){}));
-//# sourceMappingURL=03-feedback.5f1006f8.js.map
