861195009418dd7f1dfb4806e4c3c06d36296c27
Evalac <102866941+Evalac@users.noreply.github.com>
2024-07-15 11:12:29 +0000

    Deploying to gh-pages from @ Evalac/home_work_8@f5dac1f131c0471c76d9574b980e75cc7b8e98ff 🚀

---
+++ 03-feedback.1e10f67b.js
@@ -0,0 +1,2 @@
+!function(){var e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".feedback-form textarea")};e.form.addEventListener("submit",(function(e){})),e.textarea.addEventListener("input",(function(e){}))}();
+//# sourceMappingURL=03-feedback.1e10f67b.js.map
