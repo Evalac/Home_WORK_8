247bd5054a3aded20e5a22e22e48e782093605e3
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-07-16 17:31:40 +0300

    last version

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,22 +8,29 @@ const formData = {};
 
 refs.form.addEventListener('submit', onSubmit);
 refs.form.addEventListener('input', throttle(onFormEvent, 500));
+pasteTextOnLocalStorage();
 
 function onFormEvent(evt) {
   formData[evt.target.name] = evt.target.value; // створює ключ в обекті і додає до нього значення
   // console.log(formData);
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-  pasteTextOnLocalStorage(evt);
 }
 
-function pasteTextOnLocalStorage(evt) {
+function pasteTextOnLocalStorage() {
   const savedMessage = JSON.parse(localStorage.getItem('feedback-form-state'));
-  evt.target.value = savedMessage;
+  if (savedMessage) {
+    Object.entries(savedMessage).forEach(([name, value]) => {
+      if (refs.form.elements[name]) {
+        refs.form.elements[name].value = value;
+        formData[name] = value;
+      }
+    });
+  }
 }
 
 function onSubmit(evt) {
   evt.preventDefault();
-
+  console.log(formData);
   evt.target.reset(); // ресет форми после сабміта
 
   localStorage.removeItem('feedback-form-state');
